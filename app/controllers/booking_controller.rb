55d2bf09ce228453bea3433248403995c8989495
Kaushik-08 <ckaushicksingh2003@gmail.com>
2022-11-17 15:01:46 +0530

    controller

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -17,32 +17,32 @@ class BookingController < ApplicationController
     def book
       booking_seats= Booking.where(movie_id: params[:allot_movie_id], theater_id: params[:allot_theater_id]).pluck(:quantity).inject(0) {|num,x| num + x }
       unless (booking_seats + params[:seats].to_i ) < 120
-        # debugger
         flash.alert = "Your signin attempt was invalid, Please retry."
         redirect_to request.referer
-     
-      else 
-       screen = Booking.create!(
-          quantity: params[:seats], date_of_booking: params[:booking_date], movie_id: params[:allot_movie_id], theater_id: params[:allot_theater_id], show_timing_id: params[:allot_show_timing_id], user_id: current_user.id
-        )
-        screen.save!
-        redirect_to "/bookings/index"     
+          else 
+            screen = Booking.create!(
+              quantity: params[:seats], date_of_booking: params[:booking_date], movie_id: params[:allot_movie_id], theater_id: params[:allot_theater_id], show_timing_id: params[:allot_show_timing_id], user_id: current_user.id
+            )
+            screen.save!
+            redirect_to "/bookings/index"     
       end
     end  
-
+    
     def sorttheater
+      # debugger
       @theaters = Theater.joins("inner join shows on theaters.id = shows.theater_id and movie_id = #{params[:movie_id]}")
       render "booking/new"
     end
 
     def view_theater
+      # debugger
       @@theater_id= params[:theater_id]
       redirect_to "/booking/#{params[:movie_id]}"
     end
 
     def theater
-      @theater_id = @@theater_id
       @theaters = Theater.where(id: @@theater_id)
+      @theater_id = @@theater_id
       render "booking/new"
     end
 
